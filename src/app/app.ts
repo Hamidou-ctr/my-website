580bd678dcf1b2716197d09058988a0e6b619b75
Hamidou-ctr <hamiduguinea@gmail.com>
2026-04-08 11:52:55 +0200

    feat: add AboutMe component and integrate into app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Home } from "./components/home/home";
+import { AboutMe } from "./components/about-me/about-me";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home],
+  imports: [RouterOutlet, Header, Home, AboutMe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
