8a3cae69ea5c1b8254869726dbba14d19634de93
Hamidou-ctr <hamiduguinea@gmail.com>
2026-04-09 12:19:07 +0200

    feat: enhance header navigation with smooth scrolling and active section highlighting

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -10,6 +10,16 @@ import { CommonModule } from '@angular/common';
 })
 export class Header {
   isMenuOpen = false;
+  activeSection = '';
+
+  hasUserScrolled = false;
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      this.hasUserScrolled = true;
+      this.checkActiveSection();
+    });
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -21,6 +31,25 @@ export class Header {
 
   scrollTo(section: string) {
     this.closeMenu();
-  document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
-}
+    this.activeSection = section; // 🔥 sofort aktiv
+    document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  checkActiveSection() {
+    if (!this.hasUserScrolled) return;
+
+    const sections = ['about-me', 'skills', 'portfolio'];
+
+    for (let section of sections) {
+      const el = document.getElementById(section);
+      if (!el) continue;
+
+      const rect = el.getBoundingClientRect();
+
+      if (rect.top <= 150 && rect.bottom >= 150) {
+        this.activeSection = section;
+        break;
+      }
+    }
+  }
 }
