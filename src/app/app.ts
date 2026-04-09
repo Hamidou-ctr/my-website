8954b93337139a81af8429d9b5c35010bb49b547
Hamidou-ctr <hamiduguinea@gmail.com>
2026-04-09 13:08:23 +0200

    feat: add MySkills component and integrate into app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { Home } from "./components/home/home";
-import { AboutMe } from "./components/about-me/about-me";
+import { Home } from './components/home/home';
+import { AboutMe } from './components/about-me/about-me';
+import { MySkills } from './components/my-skills/my-skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home, AboutMe],
+  standalone: true,
+  imports: [RouterOutlet, Header, Home, AboutMe, MySkills],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/my-skills/my-skills.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-skills',
+  imports: [],
+  templateUrl: './my-skills.html',
+  styleUrl: './my-skills.css',
+})
+export class MySkills {}
