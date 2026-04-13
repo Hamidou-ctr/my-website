35f7298165f9d7660775d98dcc2238f830078ff9
Hamidou-ctr <hamiduguinea@gmail.com>
2026-04-13 18:23:18 +0200

    feat: implement Project component and integrate into Portfolio section

--- src/app/components/portfolio/portfolio.ts
+++ src/app/components/portfolio/portfolio.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Project } from './project/project';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [Project],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.css',
 })
