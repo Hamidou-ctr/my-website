ea292aa34507b5f657851b5fa423173857228bf0
Hamidou-ctr <hamiduguinea@gmail.com>
2026-04-16 17:52:20 +0200

    feat: implement Project component with dynamic project data and responsive layout

--- src/app/components/portfolio/project/project.ts
+++ src/app/components/portfolio/project/project.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
   templateUrl: './project.html',
   styleUrl: './project.css',
 })
-export class Project {}
+export class Project {
+  protected readonly projects = [
+    {
+      name: 'Join ',
+      language: 'Angular, TypeScript, HTML, CSS',
+      description:
+        '    Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.        ',
+      imageUrl: 'img/join.png',
+      liveTestUrl: 'https://join.hamidoudiallo.de/login.html',
+      githubUrl: 'https://github.com/username/project1',
+    },
+
+    {
+      name: 'Project 2',
+      language: 'Angular, TypeScript, HTML, CSS',
+      description: '    Another project description goes here.        ',
+      imageUrl: 'img/project2.png',
+      liveTestUrl: 'https://project2-live-test.com',
+      githubUrl: 'https://github.com/username/project2',
+    },
+
+    {
+      name: 'Project 3',
+      language: 'Angular, TypeScript, HTML, CSS',
+      description: '    Yet another project description goes here.        ',
+      imageUrl: 'img/project3.png',
+      liveTestUrl: 'https://project3-live-test.com',
+      githubUrl: 'https://github.com/username/project3',
+    },
+
+    {
+      name: 'Project 4',
+      language: 'Angular, TypeScript, HTML, CSS',
+      description: '    Description for Project 4 goes here.        ',
+      imageUrl: 'img/project4.png',
+      liveTestUrl: 'https://project4-live-test.com',
+      githubUrl: 'https://github.com/username/project4',
+    },
+  ];
+}
