4a0cc40722059b3552f87042333a71be6932c635
Hamidou-ctr <hamiduguinea@gmail.com>
2026-05-01 10:51:41 +0200

    feat: add scroll functionality to 'Get in touch' buttons in MySkills component

--- src/app/components/my-skills/my-skills.ts
+++ src/app/components/my-skills/my-skills.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScrollService } from '../../service/scroll-service';
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './my-skills.html',
   styleUrl: './my-skills.css',
 })
-export class MySkills {}
+export class MySkills {
+  constructor(private scrollService: ScrollService) {}
+
+  scrollTo(section: string) {
+    this.scrollService.scrollTo(section);
+  }
+}
