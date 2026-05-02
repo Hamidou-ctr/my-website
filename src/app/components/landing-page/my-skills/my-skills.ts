5f3b61d5c7d128ef2b4745336984c10930b27eb0
Hamidou-ctr <hamiduguinea@gmail.com>
2026-05-02 17:18:23 +0200

    feat: update MySkills component to use routerLink for navigation instead of scroll service

--- src/app/components/landing-page/my-skills/my-skills.ts
+++ src/app/components/landing-page/my-skills/my-skills.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScrollService } from '../../../service/scroll-service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './my-skills.html',
   styleUrl: './my-skills.css',
 })
 export class MySkills {
-  constructor(private scrollService: ScrollService) {}
-
-  scrollTo(section: string) {
-    this.scrollService.scrollTo(section);
-  }
 }
