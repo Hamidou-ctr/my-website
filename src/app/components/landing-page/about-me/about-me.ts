e2324345949ceb2164a9db3bee1f83cc29fd7c56
Hamidou-ctr <hamiduguinea@gmail.com>
2026-05-05 18:14:39 +0200

    feat: enhance 'About me' section with translation support and update translation service

--- src/app/components/landing-page/about-me/about-me.ts
+++ src/app/components/landing-page/about-me/about-me.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslatePipe } from '../../../service/translate.pipe';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ,TranslatePipe],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
 })
--- src/app/service/translation.service.ts
+++ src/app/service/translation.service.ts
@@ -31,20 +31,32 @@ export class TranslationService {
     'home.lets_talk': { EN: "Let's talk!", DE: 'Lass uns reden!' },
     'home.scroll_down': { EN: 'Scroll down', DE: 'Nach unten' },
 
-    // About Me
+    // About Me Section
     'about.title': { EN: 'About me', DE: 'Über mich' },
     'about.description': {
-      EN: 'I am a passionate frontend developer...',
-      DE: 'Ich bin ein leidenschaftlicher Frontend Entwickler...',
+      EN: "Hi, I'm a german speaking Frontend Developer living in Wuppertal. Motivated by the limitless opportunities within IT, I am excited about crafting visually captivating and intuitive websites and applications.",
+      DE: 'Hallo, ich bin ein deutschsprachiger Frontend Entwickler aus Wuppertal. Motiviert durch die grenzenlosen Möglichkeiten in der IT, bin ich begeistert davon, visuell fesselnde und intuitive Websites und Anwendungen zu entwickeln.',
+    },
+    'about.flexible_work': {
+      EN: 'Flexible in terms of working environments, I can work effectively both on-site in Wuppertal and remotely.',
+      DE: 'Flexibel in Bezug auf Arbeitsumgebungen kann ich sowohl vor Ort in Wuppertal als auch remote effektiv arbeiten.',
+    },
+    'about.open_minded': {
+      EN: 'I am open-minded and always looking for personal challenges to constantly improve my knowledge and skills.',
+      DE: 'Ich bin aufgeschlossen und suche immer nach persönlichen Herausforderungen, um mein Wissen und meine Fähigkeiten ständig zu verbessern.',
+    },
+    'about.problem_solving': {
+      EN: "In my profession, programming isn't just about writing code; it's a creative form of problem-solving. I take pride in my ability to distill complex technical challenges into simple, user-friendly solutions. This way, I help you achieve your goals and bring your visions to life.",
+      DE: 'In meinem Beruf geht es beim Programmieren nicht nur um das Schreiben von Code; es ist eine kreative Form der Problemlösung. Ich bin stolz auf meine Fähigkeit, komplexe technische Herausforderungen in einfache, benutzerfreundliche Lösungen zu destillieren. Auf diese Weise helfe ich Ihnen, Ihre Ziele zu erreichen und Ihre Visionen zum Leben zu erwecken.',
     },
 
-    // Skills
+    // Skills Section
     'skills.title': { EN: 'My Skills', DE: 'Meine Fähigkeiten' },
 
-    // Portfolio
+    // Portfolio Section
     'portfolio.title': { EN: 'My Projects', DE: 'Meine Projekte' },
 
-    // Contact
+    // Contact Section
     'contact.title': { EN: 'Contact me', DE: 'Kontaktieren Sie mich' },
     'contact.name': { EN: 'Name', DE: 'Name' },
     'contact.email': { EN: 'Email', DE: 'E-Mail' },
